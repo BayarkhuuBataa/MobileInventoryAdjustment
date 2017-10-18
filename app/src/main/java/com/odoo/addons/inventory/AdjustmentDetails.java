dfd7bfef371e8052f057d2b78065059606b0b0b7
sarul-odo <odo>
2017-10-18 15:56:07 +0800

    add item

--- app/src/main/java/com/odoo/addons/inventory/AdjustmentDetails.java
+++ app/src/main/java/com/odoo/addons/inventory/AdjustmentDetails.java
@@ -6,22 +6,18 @@ import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.design.widget.TabLayout;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.odoo.App;
 import com.odoo.R;
 import com.odoo.addons.customers.utils.ShareUtil;
+import com.odoo.addons.inventory.models.ProductProduct;
 import com.odoo.addons.inventory.models.StockInventory;
-import com.odoo.base.addons.ir.feature.OFileManager;
+import com.odoo.addons.inventory.models.StockInventoryLine;
 import com.odoo.core.orm.ODataRow;
 import com.odoo.core.orm.OModel;
 import com.odoo.core.orm.OValues;
@@ -29,18 +25,14 @@ import com.odoo.core.orm.fields.OColumn;
 import com.odoo.core.rpc.helper.ODomain;
 import com.odoo.core.support.OdooCompatActivity;
 import com.odoo.core.utils.OAlert;
-import com.odoo.core.utils.OControls;
 import com.odoo.core.utils.OResource;
 import com.odoo.core.utils.OStringColorUtil;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import odoo.controls.ExpandableListControl;
 import odoo.controls.OField;
 import odoo.controls.OForm;
-
-import static com.odoo.core.utils.OAlert.showError;
 
 /**
  * Created by ko on 10/5/17.
@@ -51,28 +43,23 @@ public class AdjustmentDetails extends OdooCompatActivity
         OField.IOnFieldValueChangeListener {
 
     public static final String TAG = Adjustments.class.getSimpleName();
-    private static final int REQUEST_ADD_ITEMS = 0;
+    public static final int REQUEST_ADD_ITEMS = 323;
 
     private Bundle extras;
-    private StockInventory stockInventory;
     private ODataRow record = null;
-    private List<ODataRow> recordLine = null;
-    private ImageView userImage = null;
+    private List<ODataRow> recordLine;
     private OForm mForm;
     private App app;
     private Boolean mEditMode = false;
     private Menu mMenu;
-    private OFileManager fileManager;
-    private String newImage = null;
     private CollapsingToolbarLayout collapsingToolbarLayout;
     private Toolbar toolbar;
-    private Context mContext;
-    private OnStockInventoryChangeUpdate onStockInventoryChangeUpdate;
+    private StockInventory stockInventory;
+    private StockInventoryLine stockInventoryLine;
+    private ProductProduct productProduct;
     private ExpandableListControl mList;
-    private List<Object> objects = new ArrayList<Object>();
-    private ExpandableListControl.ExpandableListAdapter mAdapter;
-    private TabLayout tabLayout;
-    private LinearLayout layoutAddItem;
+    private Context mContext;
+    private OnStockInventoryChangeUpdate onSIChangeUpdate;
 
 
     @Override
@@ -80,33 +67,28 @@ public class AdjustmentDetails extends OdooCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.stock_inventory_detail);
 
-        mContext = getApplicationContext();
-
-        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
-        tabLayout.addTab(tabLayout.newTab().setText("Inventory Details"));
-        layoutAddItem = (LinearLayout) findViewById(R.id.layoutAddItem);
-        layoutAddItem.setOnClickListener(this);
-
-        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.stock_inventory_collapsing_toolbar);
+        collapsingToolbarLayout = (CollapsingToolbarLayout)
+                findViewById(R.id.stock_inventory_collapsing_toolbar);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        fileManager = new OFileManager(this);
         if (toolbar != null)
-            collapsingToolbarLayout.setTitle("New");
+            collapsingToolbarLayout.setTitle("");
         if (savedInstanceState != null) {
             mEditMode = true;
         }
         app = (App) getApplicationContext();
         stockInventory = new StockInventory(this, null);
+        stockInventoryLine = new StockInventoryLine(this, null);
+        productProduct = new ProductProduct(this, null);
+
         extras = getIntent().getExtras();
         if (!hasRecordInExtra())
             mEditMode = true;
         setupToolbar();
-        ExtendableAdapter();
     }
 
     private boolean hasRecordInExtra() {
@@ -124,7 +106,6 @@ public class AdjustmentDetails extends OdooCompatActivity
         if (record != null) {
             color = OStringColorUtil.getStringColor(this, record.getString("name"));
         }
-        mForm = (OForm) findViewById(R.id.stockInventoryForm);
         if (edit) {
             if (!hasRecordInExtra()) {
                 collapsingToolbarLayout.setTitle("New");
@@ -137,61 +118,40 @@ public class AdjustmentDetails extends OdooCompatActivity
     }
 
     private void setupToolbar() {
+        mForm = (OForm) findViewById(R.id.stockInventoryForm);
         if (!hasRecordInExtra()) {
             setMode(mEditMode);
             mForm.setEditable(mEditMode);
             mForm.initForm(null);
         } else {
             int rowId = extras.getInt(OColumn.ROW_ID);
+
             record = stockInventory.browse(rowId);
             recordLine = record.getO2MRecord("line_ids").browseEach();
-            checkControls();
+
             setMode(mEditMode);
             mForm.setEditable(mEditMode);
             mForm.initForm(record);
             collapsingToolbarLayout.setTitle(record.getString("name"));
+
+//            drawLine();
         }
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.layoutAddItem:
-                loadActivity();
-                finish();
-                break;
+
         }
     }
 
-    private void loadActivity() {
-        Intent intent = new Intent(this, AddProductLineWizard.class);
-        Bundle extra = new Bundle();
-//        for (String key : recordLine.keySet()) {
-//            extra.putFloat(key, recordLine.get(key));
-//        }
-        intent.putExtras(extra);
-        startActivityForResult(intent, REQUEST_ADD_ITEMS);
-
-//        Intent intent = new Intent(this, AddProductLineWizard.class);
-//        startActivity(intent);
-
-    }
-
-    private void checkControls() {
-        findViewById(R.id.name).setOnClickListener(this);
-//        findViewById(R.id.website).setOnClickListener(this);
-//        findViewById(R.id.email).setOnClickListener(this);
-//        findViewById(R.id.phone_number).setOnClickListener(this);
-//        findViewById(R.id.mobile_number).setOnClickListener(this);
-    }
-
     private void setColor(int color) {
-        mForm.setIconTintColor(Color.RED);
+        mForm.setIconTintColor(color);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        onStockInventoryChangeUpdate = new OnStockInventoryChangeUpdate();
+        onSIChangeUpdate = new OnStockInventoryChangeUpdate();
         ODomain domain = new ODomain();
         switch (item.getItemId()) {
             case android.R.id.home:
@@ -202,14 +162,14 @@ public class AdjustmentDetails extends OdooCompatActivity
                 if (values != null) {
                     if (record != null) {
                         stockInventory.update(record.getInt(OColumn.ROW_ID), values);
-                        onStockInventoryChangeUpdate.execute(domain);
+                        onSIChangeUpdate.execute(domain);
                         Toast.makeText(this, R.string.toast_information_saved, Toast.LENGTH_LONG).show();
                         mEditMode = !mEditMode;
                         setupToolbar();
                     } else {
                         final int row_id = stockInventory.insert(values);
+                        onSIChangeUpdate.execute(domain);
                         if (row_id != OModel.INVALID_ROW_ID) {
-                            onStockInventoryChangeUpdate.execute(domain);
                             Toast.makeText(this, R.string.stock_inventory_created, Toast.LENGTH_LONG).show();
                             finish();
                         }
@@ -223,7 +183,6 @@ public class AdjustmentDetails extends OdooCompatActivity
                     setMode(mEditMode);
                     mForm.setEditable(mEditMode);
                     mForm.initForm(record);
-//                    setCustomerImage();
                 } else {
                     finish();
                 }
@@ -241,7 +200,6 @@ public class AdjustmentDetails extends OdooCompatActivity
                             @Override
                             public void onConfirmChoiceSelect(OAlert.ConfirmType type) {
                                 if (type == OAlert.ConfirmType.POSITIVE) {
-                                    // Deleting record and finishing activity if success.
                                     if (stockInventory.delete(record.getInt(OColumn.ROW_ID))) {
                                         Toast.makeText(AdjustmentDetails.this, R.string.toast_record_deleted,
                                                 Toast.LENGTH_SHORT).show();
@@ -250,28 +208,10 @@ public class AdjustmentDetails extends OdooCompatActivity
                                 }
                             }
                         });
+
                 break;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_stock_inventory_detail, menu);
-        mMenu = menu;
-        setMode(mEditMode);
-        return true;
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putBoolean(String.valueOf(false), mEditMode);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
     }
 
     private class OnStockInventoryChangeUpdate extends AsyncTask<ODomain, Void, Void> {
@@ -303,42 +243,25 @@ public class AdjustmentDetails extends OdooCompatActivity
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_stock_inventory_detail, menu);
+        mMenu = menu;
+        setMode(mEditMode);
+        return true;
+    }
+
+    @Override
     public void onFieldValueChange(OField field, Object value) {
-//        if (record == null && field.getFieldName().equals("technic_id")) {
-//            ODataRow techVal = (ODataRow) value;
-//            technicSync(techVal.getString("id"));
+    }
 
-        }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(String.valueOf(false), mEditMode);
+    }
 
-    private void ExtendableAdapter() {
-        try {
-            mList = (ExpandableListControl) findViewById(R.id.inventory_detail_lines);
-            mList.setVisibility(View.VISIBLE);
-//            mList.setOnClickListener();
-            if (extras != null && record != null) {
-                if (recordLine.size() > 0) {
-                    objects.addAll(recordLine);
-                }
-            }
-            mAdapter = mList.getAdapter(R.layout.inventory_details, objects,
-                    new ExpandableListControl.ExpandableListAdapterGetViewListener() {
-                        @Override
-                        public View getView(int position, View mView, ViewGroup parent) {
-                            ODataRow row = (ODataRow) mAdapter.getItem(position);
-                            Log.d(TAG, "row : " + row);
-                            ODataRow prod = row.getM2ORecord("product_id").browse();
-                            ODataRow loc = row.getM2ORecord("location_id").browse();
-                            OControls.setText(mView, R.id.edit_product, prod.getString("name"));
-                            OControls.setText(mView, R.id.edit_location, loc.getString("name"));
-                            OControls.setText(mView, R.id.edit_check_qty, String.format("%.2f", row.getFloat("theoretical_qty")));
-                            OControls.setText(mView, R.id.edit_real_qty, String.format("%.2f", row.getFloat("product_qty")));
-                            return mView;
-                        }
-                    });
-            mAdapter.notifyDataSetChanged(objects);
-        } catch (Exception ex) {
-            showError(this, "Error" + ex.toString());
-            Log.e(TAG, "ERROR", ex);
-        }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
