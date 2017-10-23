2feae5809bf859fa8e8235978e721acbbb3911a0
sarul-odo <odo>
2017-10-23 22:05:01 +0800

    server update line except create

--- app/src/main/java/com/odoo/addons/inventory/AdjustmentDetails.java
+++ app/src/main/java/com/odoo/addons/inventory/AdjustmentDetails.java
@@ -1,5 +1,7 @@
 package com.odoo.addons.inventory;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
@@ -7,9 +9,11 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
@@ -26,9 +30,11 @@ import com.odoo.core.orm.fields.OColumn;
 import com.odoo.core.rpc.helper.ODomain;
 import com.odoo.core.support.OdooCompatActivity;
 import com.odoo.core.utils.OAlert;
+import com.odoo.core.utils.OControls;
 import com.odoo.core.utils.OResource;
 import com.odoo.core.utils.OStringColorUtil;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -49,7 +55,7 @@ public class AdjustmentDetails extends OdooCompatActivity
 
     private Bundle extras;
     private ODataRow record = null;
-    private List<ODataRow> recordLine;
+    private List<ODataRow> recordLine = new ArrayList<>();
     private OForm mForm;
     private App app;
     private Boolean mEditMode = false;
@@ -65,6 +71,8 @@ public class AdjustmentDetails extends OdooCompatActivity
     private LinearLayout layoutAddItem;
     private HashMap<String, Float> lineValues = new HashMap<String, Float>();
     private HashMap<String, Integer> lineIds = new HashMap<String, Integer>();
+    private ExpandableListControl.ExpandableListAdapter mAdapter;
+    private List<Object> objects = new ArrayList<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -85,6 +93,10 @@ public class AdjustmentDetails extends OdooCompatActivity
             mEditMode = true;
         }
         app = (App) getApplicationContext();
+
+        mList = (ExpandableListControl) findViewById(R.id.inventory_detail_lines);
+        mList.setVisibility(View.VISIBLE);
+
         stockInventory = new StockInventory(this, null);
         stockInventoryLine = new StockInventoryLine(this, null);
         productProduct = new ProductProduct(this, null);
@@ -130,11 +142,8 @@ public class AdjustmentDetails extends OdooCompatActivity
             mForm.setEditable(mEditMode);
             mForm.initForm(null);
         } else {
-            int rowId = extras.getInt(OColumn.ROW_ID);
-
-            record = stockInventory.browse(rowId);
-            recordLine = record.getO2MRecord("line_ids").browseEach();
-
+            init();
+            drawLines(recordLine);
             setMode(mEditMode);
             mForm.setEditable(mEditMode);
             mForm.initForm(record);
@@ -143,11 +152,19 @@ public class AdjustmentDetails extends OdooCompatActivity
         }
     }
 
+    private void init() {
+        int rowId = extras.getInt(OColumn.ROW_ID);
+        record = stockInventory.browse(rowId);
+        recordLine = record.getO2MRecord("line_ids").browseEach();
+    }
+
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.layoutAddItem:
                 if (mForm.getValues() != null) {
+                    getLineProducts();
+
                     Intent intent = new Intent(this, AddProductLineWizard.class);
                     Bundle extra = new Bundle();
                     for (String key : lineValues.keySet()) {
@@ -157,6 +174,18 @@ public class AdjustmentDetails extends OdooCompatActivity
                     startActivityForResult(intent, REQUEST_ADD_ITEMS);
                 }
                 break;
+        }
+    }
+
+    private void getLineProducts() {
+        if (recordLine.size() > 0) {
+            for (ODataRow line : recordLine) {
+                int product_id = productProduct.selectServerId(line.getInt("product_id"));
+                if (product_id != 0) {
+                    lineValues.put(product_id + "", line.getFloat("product_qty"));
+                    lineIds.put(product_id + "", line.getInt("id"));
+                }
+            }
         }
     }
 
@@ -175,6 +204,12 @@ public class AdjustmentDetails extends OdooCompatActivity
             case R.id.menu_stock_inventory_save:
                 OValues values = mForm.getValues();
                 if (values != null) {
+                    List ids = new ArrayList();
+                    for (ODataRow row : recordLine) {
+                        OValues oValues = new OValues();
+                        ids.add(row.getInt("_id"));
+                    }
+                    values.put("line_ids", ids);
                     if (record != null) {
                         stockInventory.update(record.getInt(OColumn.ROW_ID), values);
                         onSIChangeUpdate.execute(domain);
@@ -235,14 +270,8 @@ public class AdjustmentDetails extends OdooCompatActivity
         protected Void doInBackground(ODomain... params) {
             if (app.inNetwork()) {
                 ODomain domain = params[0];
-                domain.add("&");
-                domain.add("|");
-                domain.add("state","=","draft");
-                domain.add("state","=","confirm");
-                domain.add("filter","!=","none");
-                List<ODataRow> rows = stockInventory.select(null, "id = ?", new String[]{"0"});
-                for (ODataRow row : rows) {
-                    stockInventory.quickCreateRecord(row);
+                for (ODataRow row : recordLine) {
+                    stockInventoryLine.quickCreateRecord(row);
                 }
                 stockInventory.quickSyncRecords(domain);
             }
@@ -278,5 +307,102 @@ public class AdjustmentDetails extends OdooCompatActivity
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_ADD_ITEMS && resultCode == Activity.RESULT_OK) {
+            lineValues.clear();
+            for (String key : data.getExtras().keySet()) {
+                if (data.getExtras().getFloat(key) > 0)
+                    lineValues.put(key, data.getExtras().getFloat(key));
+            }
+            OnProductChange onProductChange = new OnProductChange();
+            onProductChange.execute(lineValues);
+
+        }
     }
+
+    private void drawLines(List<ODataRow> rows) {
+        objects.clear();
+        objects.addAll(rows);
+        mAdapter = mList.getAdapter(R.layout.inventory_details, objects,
+                new ExpandableListControl.ExpandableListAdapterGetViewListener() {
+                    @Override
+                    public View getView(int position, View mView, ViewGroup parent) {
+                        ODataRow row = (ODataRow) mAdapter.getItem(position);
+                        Log.d(TAG, "row : " + row);
+
+                        OControls.setText(mView, R.id.edit_product, row.getString("product_id"));
+                        OControls.setText(mView, R.id.edit_location, row.getString("location_id"));
+                        OControls.setText(mView, R.id.edit_check_qty, String.format("%.2f", row.getFloat("theoretical_qty")));
+                        OControls.setText(mView, R.id.edit_real_qty, String.format("%.2f", row.getFloat("product_qty")));
+
+                        return mView;
+                    }
+                });
+        mAdapter.notifyDataSetChanged(objects);
+    }
+
+    private class OnProductChange extends AsyncTask<HashMap<String, Float>, Void, List<ODataRow>> {
+        private ProgressDialog progressDialog;
+        private String warning = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected List<ODataRow> doInBackground(HashMap<String, Float>... params) {
+
+            List<ODataRow> items = new ArrayList<>();
+            int inventoryId = extras.getInt(OColumn.ROW_ID);
+
+            try {
+                for (String key : params[0].keySet()) {
+                    Float qty = params[0].get(key);
+
+                    int product_row_id = productProduct.selectRowId(Integer.parseInt(key));
+
+                    OValues values = new OValues();
+                    values.put("inventory_id", inventoryId);
+                    values.put("product_id", product_row_id);
+//  product uom id -g olj tavih dutuu !!!
+                    values.put("location_id", 1);
+                    values.put("theoretical_qty", 0.0);
+                    values.put("product_qty", qty);
+//  product uom id -g olj tavih dutuu !!!
+                    values.put("product_uom_id", 1);
+                    items.add(values.toDataRow());
+
+                    if (recordLine.size() > 0) {
+                        String updateRowId = new String();
+                        for (ODataRow rec : recordLine) {
+                            int product_id = productProduct.selectServerId(rec.getInt("product_id"));
+                            if (product_id == Integer.parseInt(key)) {
+                                updateRowId = String.valueOf(rec.getString("_id"));
+                            }
+                        }
+
+                        if (updateRowId.equals("")) {
+                            stockInventoryLine.insert(values);
+                        } else {
+                            stockInventoryLine.update(Integer.parseInt(updateRowId), values);
+                        }
+                    } else {
+                        stockInventoryLine.insert(values);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return items;
+        }
+
+        @Override
+        protected void onPostExecute(List<ODataRow> row) {
+            super.onPostExecute(row);
+//            recordLine = stockInventory.select(null, "inventory_id = ?", new String[]{extras.getString(OColumn.ROW_ID)});
+            recordLine = record.getO2MRecord("line_ids").browseEach();
+            drawLines(recordLine);
+        }
+    }
+
 }
